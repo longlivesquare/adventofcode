cf54e68fa808c61c561e5c43f7df34ec097abc12
Steven Duran <stevenduran@gmail.com>
2018-01-27 16:19:45 -0800

    day7

--- day7/day7.go
+++ day7/day7.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"bufio"
+	"fmt"
 	"os"
+	"strconv"
 	"strings"
 	"unicode"
 )
@@ -17,15 +19,22 @@ func (f *floor) putOn(topF *floor) {
 	f.above = append(f.above, topF)
 }
 
-var tower = make([]*floor, 1288)
+var tower = make([]*floor, 0)
 
-func exists(s string) int {
-	for i, v := range tower {
-		if v.name == s {
-			return i
+func exists(s string, f *floor) (bool, *floor) {
+	if f == nil {
+		return false, nil
+	}
+	if f.name == s {
+		return true, f
+	}
+	for i := range f.above {
+		b, fl := exists(s, f.above[i])
+		if b {
+			return b, fl
 		}
 	}
-	return -1
+	return false, nil
 }
 
 func readFile(s string) {
@@ -39,14 +48,36 @@ func readFile(s string) {
 	for scanner.Scan() {
 		str := scanner.Text()
 		names := strings.FieldsFunc(str, fun)
+		wt, _ := strconv.Atoi(names[1])
+		bFl := &floor{names[0], wt, make([]*floor, 0)}
 
 		for i, v := range names {
-			if i == 0 || i >= 2 {
-				fl := exists(v)
-				if f1 > 0 {
-					if i == 0 {
-
+			if i == 0 {
+				b := false
+				for j := range tower {
+					b, fl := exists(v, tower[j])
+					if b {
+						fl.weight = wt
+						bFl = fl
+						fmt.Println(fl.name, "(", fl.weight, ")")
+						break
 					}
+				}
+				if !b {
+					tower = append(tower, bFl)
+				}
+			} else if i >= 2 {
+				b := false
+				for j := range tower {
+					b, fl := exists(v, tower[j])
+					if b {
+						bFl.putOn(fl)
+						tower = append(tower[:j], tower[j+1:]...)
+						break
+					}
+				}
+				if !b {
+					bFl.putOn(&floor{v, 0, make([]*floor, 0)})
 				}
 			}
 		}
@@ -63,5 +94,16 @@ func check(e error) {
 
 func main() {
 	readFile("day7.txt")
-
+	for _, v := range tower {
+		if v != nil {
+			fmt.Print(v.name)
+			for _, v2 := range v.above {
+				if v2 != nil {
+					fmt.Print(" ", v2.name)
+				}
+			}
+			fmt.Println()
+		}
+	}
+	fmt.Println(len(tower))
 }
